30971b138ca93edf9805aef767949f4b43a576d6
n0nm3 <enzocollot2543@gmail.com>
2022-12-09 18:35:27 -0500

    Test socket

--- bash/socket.sh
+++ bash/socket.sh
@@ -2,7 +2,7 @@
 
 YES=1
 
-echo > /dev/udp/localhost/22 && echo '$YES' || echo 'NOP'
-
 
 echo > /dev/tcp/localhost/22 && echo "$YES" || echo 'NOP'
+
+echo "$YES" > /dev/tcp/localhost/1500 || echo 'Na'
