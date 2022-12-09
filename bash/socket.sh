7fec709c4ce275470f87570f9c1addc79bbab97e
n0nm3 <enzocollot2543@gmail.com>
2022-12-09 18:43:40 -0500

    socket and shell made

--- bash/socket.sh
+++ bash/socket.sh
@@ -5,4 +5,5 @@ YES=1
 
 echo > /dev/tcp/localhost/22 && echo "$YES" || echo 'NOP'
 
-echo "$YES" > /dev/tcp/localhost/1500 || echo 'Na'
+echo "$YES" > /dev/tcp/localhost/1500 || echo 'Na'   #replace 1500 with the tcp port listening && localhost by the IP
+
