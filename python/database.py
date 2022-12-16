ed1e6b9a2ffc26ed3cf04813c0b4f88c7dc64bca
n0nm3 <enzocollot2543@gmail.com>
2022-12-16 16:20:09 -0500

    try with database class

--- python/database.py
+++ python/database.py
@@ -2,13 +2,28 @@ import mysql.connector
 
 
 
-mydb = mysql.connector.connect(
+db = mysql.connector.connect(
         host="localhost",
         user="kali",
-        password="kali",
-        database="Formulaire"
+        password="kali"
+        #database="Formulaire"
 )
 
+class database():
+    def __init__(self):
+        self.cursor = db.cursor
+    def get_etudiant(self):
+        etu = etu.root      #faut caller l'id de l'étudiant définit dans kivy
+        query = f"SELECT * from etudiant where NomEtu={etu}"
+        r = db.cursor.execute(query)
+        db.close
+        return r
+    def get_list(self):
+        query = f"SELECT * from etudiant"
+        r = db.cursor.execute(query)
+        db.close
+        return r
+        
 
 def main():
     cursor = db.cursor()
