011572b7952892843a76fc5d223614577a81ebdc
n0nm3 <95082158+n0nm3@users.noreply.github.com>
2022-12-18 17:25:18 +0100

    Add files via upload

---
+++ Portfolio/_footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <h1>Test h1</h1>
+
+    <h3>Test h3</h3>
+</footer>
---
+++ Portfolio/ind.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/style2.css">
+    </head>
+<body>
+<?php 
+require_once('_header.php');
+?>    
+
+trait_exis
+<p>blablabla</p>
+<?php
+require('_footer.php');
+?>
+</body>
+</html>
